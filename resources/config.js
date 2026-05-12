c9b5bbd45d45c6b1e69c9714dd113c1fe37ece68
The Buildbot <noreply@haushoppe.art>
2026-05-12 12:47:17 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/ordpool-space/ordpool/commit/bdd29846b3e0609958333390bfc4774052b37427
    GitHub Actions build: https://github.com/ordpool-space/ordpool/actions/runs/25735198755

--- resources/config.js
+++ resources/config.js
@@ -3,6 +3,6 @@
     window.__env.MEMPOOL_WEBSITE_URL = 'https://ordpool.space';
     window.__env.ITEMS_PER_PAGE = 25;
     window.__env.BASE_MODULE = 'ordpool';
-    window.__env.GIT_COMMIT_HASH = 'ba74ad7';
+    window.__env.GIT_COMMIT_HASH = 'bdd2984';
     window.__env.PACKAGE_JSON_VERSION = '3.4-dev';
   }((typeof global !== 'undefined') ? global : this));
